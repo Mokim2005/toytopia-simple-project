ef6cb2272a3f8692c0d84027c76c8f32abfd17e5
mokim <mamokim2005@gmail.com>
2025-10-25 19:15:58 +0600

    some error are clear

--- src/Components/ToyDetailsCard.jsx
+++ src/Components/ToyDetailsCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ToyDetailsCard = ({ toys }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
